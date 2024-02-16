fcd5b082499b160dc912e6ce918c4e97a2b62f4c
Sergio Picascia <sergiopicascia@libero.it>
2024-02-16 19:25:04 +0100

    added option to get multiple items, added docs

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -3,7 +3,7 @@ Dataset object.
 """
 
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, List
 import os
 import json
 from collections import defaultdict
@@ -33,10 +33,18 @@ class PartImageNetDataset(Dataset):
     Object for loading the PartImageNet dataset.
     """
 
-    def __init__(self, root, split) -> None:
-        self.root = root
-        self.split = split
+    def __init__(self, root: str, split: str = "train") -> None:
+        """Load PartImageNet dataset.
 
+        Args:
+            root (str): the folder in which the dataset is stored.
+            split (str): the split of the dataset to load.
+                Defaults to "train"; accepts also "test" and "val".
+
+        Raises:
+            ValueError: if the value of split is other than "train", "test" or "val".
+        """
+        # Load json file with data
         if split in ["train", "test", "val"]:
             with open(
                 os.path.join(root, f"annotations/{split}/{split}.json"),
@@ -45,17 +53,48 @@ class PartImageNetDataset(Dataset):
             ) as f:
                 data = json.load(f)
         else:
-            raise ValueError
+            raise ValueError(f"{split} is not a valid value for split.")
 
+        self.root = root
+        self.split = split
+
+        # Dictionary of the form image_id: image_filename
         self.img_files = {img["id"]: img["file_name"] for img in data["images"]}
 
+        # Dictionary of the form image_id: [image_annotations]
         self.annotations = defaultdict(list)
         for annotation in data["annotations"]:
             self.annotations[annotation["image_id"]].append(annotation)
 
         self.categories = data["categories"]
 
-    def __getitem__(self, index) -> Dict:
+    def __getitem__(self, index: int | slice) -> List[Dict]:
+        """Returns the indexed images.
+
+        Args:
+            index (int or slice): indexes of the images to return.
+
+        Returns:
+            List: list of images and their annotations.
+        """
+        if isinstance(index, slice):
+            return [self._get_img_data(i) for i in range(*index.indices(len(self)))]
+        if isinstance(index, int):
+            return [self._get_img_data(index)]
+
+        raise TypeError(
+            f"{type(self).__name__} indices must be integers or slices, not {type(index).__name__}."
+        )
+
+    def _get_img_data(self, index) -> Dict:
+        """Retrieve data about the image.
+
+        Args:
+            index (int): index of the image in the collection.
+
+        Returns:
+            Dict: dictionary containing the "id", "image" pixel values and its "annotations".
+        """
         img = cv2.cvtColor(
             cv2.imread(
                 os.path.join(self.root, f"images/{self.split}/{self.img_files[index]}")
@@ -65,4 +104,9 @@ class PartImageNetDataset(Dataset):
         return {"id": index, "image": img, "annotations": self.annotations[index]}
 
     def __len__(self) -> int:
+        """Return the number of images in the dataset.
+
+        Returns:
+            int: number of images in dataset.
+        """
         return len(self.img_files)
